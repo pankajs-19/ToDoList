a9727189f9cf57861b4c9aeeda9c63f9faa37b0a
S <sappankajswami32@gmail.com>
2025-07-28 21:57:25 +0530

    update host list for railway

--- todolist/settings.py
+++ todolist/settings.py
@@ -25,7 +25,15 @@ SECRET_KEY = 'django-insecure-%%d8n+wdx!v9tckg00#ss=xmngqc&o#_g_m#&v6&)%5h01mqk+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app"
+]
+
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 
 # Application definition
